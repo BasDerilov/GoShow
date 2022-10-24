237b44d0a044c3f03441c7877aefdaf3a482f4d3
Vasil Derilov <vasil.derilov@gmail.com>
2022-10-24 09:10:24 +0300

    Added enviroment variables to production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,27 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://api.themoviedb.org/3',
 };
+
+export enum ApiPaths {
+  Account = '/account',
+  Auth = '/authentication',
+  Certification = '/certification',
+  Collection = '/collection',
+  Companies = '/company',
+  Configuration = '/configuration',
+  Credit = '/credit',
+  Discover = '/discover',
+  Find = '/find',
+  Genre = '/genre',
+  Keyword = '/keyword',
+  List = '/list',
+  Movie = '/movie',
+  Network = '/network',
+  Trending = '/trending',
+  People = '/person',
+  Reviews = '/review',
+  Search = '/search',
+  Tv = '/tv',
+  Watch = '/watch',
+}
